86ee44e4881d00046d6ab26390aee76c1c19f19a
rrc83 <100827785+rrc83@users.noreply.github.com>
2022-06-09 13:13:14 +0200

    Update validar-campos.js

--- middleware/validar-campos.js
+++ middleware/validar-campos.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const {validationResult} = require('express-validator');
+const moment = require('moment');
 
 const validarCampos = (req,res=express.response,next) =>{
 
@@ -14,6 +15,21 @@ const validarCampos = (req,res=express.response,next) =>{
     next();
 }
 
+const validarStartEnd = (req,res=express.response,next) =>{
+    const{start,end} = req.body;
+    const mS = moment(start);
+    const mE = moment(end);
+
+    if (mS.isSameOrAfter(mE)){   
+        return res.status(400).json({
+            ok:false,
+            msg:'La fecha de fin debe ser mayor a la fecha de inicio'
+        });
+    }
+    next();
+}
+
 module.exports = {
-    validarCampos
+    validarCampos,
+    validarStartEnd
 }
